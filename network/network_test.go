4518780af6f6ff36396b8ee1b1ed4bbf771eeec1
Kravchenko Danylo <kravchel16@gmail.com>
2019-11-23 23:18:37 +0200

    add benchmark via opened tcp connection

--- network/network_test.go
+++ network/network_test.go
@@ -1,13 +1,37 @@
 package network
 
-import "testing"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"sync/atomic"
+	"testing"
+	"time"
+)
 
-func TestOpenConnection(t *testing.T) {
-	nm := NewNetworkManager(NetConfig{
-		Host: "localhost",
-		Port: "8080",
-		Type: "tcp",
-	})
+var rps uint64
 
-	nm.OpenConnection()
+func TestConnection(t *testing.T) {
+	for i := 0; i<50; i++ {
+		go func() {
+			conn, err := net.Dial("tcp", ":8964")
+			if err != nil {
+				t.Fatal(err)
+			}
+			for {
+				fmt.Fprintf(conn, "PING\r\n")
+				_, err := bufio.NewReader(conn).ReadBytes('\n')
+				if err != nil {
+					t.Fatal(err)
+				}
+				atomic.AddUint64(&rps, 1)
+			}
+		}()
+	}
+	select {
+	case <- time.After(5 * time.Second):
+		log.Println(rps/5)
+		return
+	}
 }
